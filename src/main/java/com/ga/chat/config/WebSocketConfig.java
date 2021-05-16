ed4dd35a993ed1dacee9514c109b66ea5d90f27b
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-16 16:45:16 -0500

    configureMessageBroker method in websocket configuration

--- src/main/java/com/ga/chat/config/WebSocketConfig.java
+++ src/main/java/com/ga/chat/config/WebSocketConfig.java
@@ -8,8 +8,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
 
 }
