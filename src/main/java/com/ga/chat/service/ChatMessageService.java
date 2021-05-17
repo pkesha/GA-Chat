8c43877b27cb0a3f6850fc5f655980f26a9cbccc
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-17 16:45:46 -0500

    added get user method to chate message service

--- src/main/java/com/ga/chat/service/ChatMessageService.java
+++ src/main/java/com/ga/chat/service/ChatMessageService.java
@@ -2,7 +2,10 @@ package com.ga.chat.service;
 
 import com.ga.chat.model.User;
 import com.ga.chat.repository.*;
+import com.ga.chat.security.MyUserDetails;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,17 +17,18 @@ public class ChatMessageService {
     private final UserRepository userRepository;
 
     @Autowired
-
     public ChatMessageService(ChatMessageRepository chatMessageRepository, ConversationRepository conversationRepository, UserRepository userRepository) {
         this.chatMessageRepository = chatMessageRepository;
         this.conversationRepository = conversationRepository;
         this.userRepository = userRepository;
     }
 
-//    public User getUser() {
-//
-//    }
+    public User getUser(){
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder
+        .getContext()
+        .getAuthentication()
+        .getPrincipal();
 
-    /***************ChatMessages**************/
-//    public List<Messages> getMessages
+        return userDetails.getUser();
+    }
 }
