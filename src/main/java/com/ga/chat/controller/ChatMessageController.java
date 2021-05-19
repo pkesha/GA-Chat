48efb68ceb1caf30cc93e13a91ee5f5d61b750eb
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-18 20:51:21 -0500

    added post and get (for individual message) methods

--- src/main/java/com/ga/chat/controller/ChatMessageController.java
+++ src/main/java/com/ga/chat/controller/ChatMessageController.java
@@ -9,14 +9,21 @@ import com.ga.chat.security.MyUserDetails;
 import com.ga.chat.service.ChatMessageService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
 @Controller
+@RequestMapping(path = "/api")
 public class ChatMessageController {
 
     private final ChatMessageService chatMessageService;
@@ -31,4 +38,13 @@ public class ChatMessageController {
         return chatMessageService.getMessages();
     }
 
+    @GetMapping("/chatmessages/{chatmessageId}")
+    public ChatMessage getChatMessage(@PathVariable Long chatMessageId) {
+        return chatMessageService.getChatMessage(chatMessageId);
+    }
+
+    @PostMapping("/chatmessages")
+    public ResponseEntity<ChatMessage> sendMessage(@RequestBody ChatMessage chatMessage) {
+        return new ResponseEntity<>(chatMessageService.sendMessage(chatMessage), HttpStatus.CREATED);
+    }
 }
