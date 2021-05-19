9898c6086418743ad63773b1fd811dd5a995d64d
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-19 09:59:25 -0500

    Added new constuctor to user model to accept name

--- src/main/java/com/ga/chat/model/User.java
+++ src/main/java/com/ga/chat/model/User.java
@@ -29,6 +29,13 @@ public class User {
     public User() {
     }
 
+    public User(String userName, String password, String firstName, String lastName) {
+        this.userName = userName;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
