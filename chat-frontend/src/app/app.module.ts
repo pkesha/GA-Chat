5ec10b450503e1c826fea452f53d15a7e06692c2
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-20 11:47:06 -0500

    added user login method in login component

--- chat-frontend/src/app/app.module.ts
+++ chat-frontend/src/app/app.module.ts
@@ -3,16 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './user/login/login.component';
-import { RegisterComponent } from './user/register/register.component';
 import {FormsModule} from "@angular/forms";
 import {UserModule} from "./user/user.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
   ],
     imports: [
         BrowserModule,
--- chat-frontend/src/app/user/login/login.component.ts
+++ chat-frontend/src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user/user.service";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public userName: string | undefined;
+  public password: string | undefined;
+  public token: any;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
+
+  loginUser() {
+    const user = {
+      userName: this.userName,
+      password: this.password
+    }
+
+    this.token = this.userService.loginUser(user);
+  }
+
 
 }
