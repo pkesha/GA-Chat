0feaa3fa1858223e9956550a7dc86c5920edf567
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-20 11:35:58 -0500

    added login and register components to routes in app-routing

--- chat-frontend/src/app/app-routing.module.ts
+++ chat-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./user/register/register.component";
+import {LoginComponent} from "./user/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
