07d80c927533116d0cec4471e3780e9a68e880fe
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-20 14:11:48 -0500

    Added get messages method to chat service

--- chat-frontend/src/app/services/chat/chat.service.ts
+++ chat-frontend/src/app/services/chat/chat.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
+const url = 'http://localhost:9092/api/messages';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  message: string | undefined;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMessages() {
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders( {
+        Authorization: `Bearer ${token}`
+      })
+    };
+
+    return this.http
+      .get(`${url}`, requestOptions);
+  }
 }
