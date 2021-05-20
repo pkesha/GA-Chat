a978ff180fa87c4360e0486a7431e2cb4187ca8f
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-20 10:18:32 -0500

    Added register user method for register ts file

--- chat-frontend/src/app/register/register.component.ts
+++ chat-frontend/src/app/register/register.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  public userName: String;
+  public password: String;
+  public firstName: String;
+  public lastName: String;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  registerUser(): void {
+    const newUser = {
+      userName: this.userName,
+      password: this.password,
+      firstName: this.firstName,
+      lastName: this.lastName
+    };
+
   }
 
 }
