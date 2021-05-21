efa2a4e51aa3d151acce3d198ed4859f5f4e13b4
Parham Keshavarzi <parham.keshavarzi7117@gmail.com>
2021-05-20 23:21:53 -0500

    Added send, delete get all messages methods in chatmessages component and chatbox component

--- chat-frontend/src/app/chatbox/chatbox.component.ts
+++ chat-frontend/src/app/chatbox/chatbox.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ChatService} from "../services/chat/chat.service";
 
 @Component({
   selector: 'app-chatbox',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatbox.component.css']
 })
 export class ChatboxComponent implements OnInit {
+  public chatMessages: [] | undefined;
+  public message: string | undefined;
 
-  constructor() { }
+  constructor(private chatService : ChatService) { }
 
   ngOnInit(): void {
+    if(this.chatMessages === null) {
+
+    } else {
+
+    }
   }
 
+  getChatMessages(): void {
+    this.chatService.getMessages()
+      .subscribe(messages => {
+        // @ts-ignore
+        this.chatMessages = messages;
+        console.log(this.chatMessages);
+      });
+  }
+
+  sendMessage(): any {
+    let newChatMessage: any = {
+      message: this.message,
+      date: Date.now()
+    };
+
+    this.chatService.sendChatMessage(newChatMessage)
+      .subscribe((response: any) => {
+        console.log(response)
+        this.getChatMessages();
+      })
+  }
 }
--- chat-frontend/src/app/chatmessages/chatmessages.component.ts
+++ chat-frontend/src/app/chatmessages/chatmessages.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import {ChatService} from "../services/chat/chat.service";
 
 @Component({
   selector: 'app-chatmessages',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatmessages.component.css']
 })
 export class ChatmessagesComponent implements OnInit {
+  @Input() message : any;
 
-  constructor() { }
+  @Output() deleteItemEvent = new EventEmitter<any>();
+
+  constructor(private chatService: ChatService) { }
 
   ngOnInit(): void {
+  }
+
+  deleteMessage(chatMessageId: number): void {
+    this.chatService.deleteChatMessage(chatMessageId)
+      .subscribe(response => {
+        console.log(response + "message id " + chatMessageId);
+        this.deleteItemEvent.emit();
+      });
   }
 
 }
